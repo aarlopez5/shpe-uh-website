7f24d31e77d6cd9b18cdbbd021e41a5bb7b2c5cf
Aaron Lopez <aarlopez5@gmail.com>
2026-04-03 20:15:40 -0500

    Changed fits the our between s and t in about page

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -31,7 +31,8 @@ export function ScrollTransitionHero() {
 
         <div className="relative z-10 flex min-h-screen items-center justify-center w-full">
           <h1 className="leading-none font-bold flex flex-col" style={{ fontFamily: 'Work Sans, sans-serif' }}>
-            <span className="text-[#1a2858] text-[58px] md:text-[116px] left-14 md:left-28 italic relative">our</span>
+            <span className="text-[#1a2858] text-[58px] md:text-[116px] top-2 md:top-5 left-8 md:left-12 italic relative">our</span>
+            
             <span 
               className="px-5 pb-5 -mb-5 -mt-6 md:-mt-13 text-[150px] md:text-[298px] italic" 
               style={{ 
